9d78a11b200e6ea94c25b6a4845f8de9d82add78
info@noudadrichem.com <info@noudadrichem.com>
2019-06-19 17:43:01 +0200

    removed unused function

--- src/main/java/persistence/PostgresBaseDao.java
+++ src/main/java/persistence/PostgresBaseDao.java
@@ -1,35 +1,28 @@
 package persistence;
 
 import java.sql.*;
-// import javax.naming.InitialContext;
-// import javax.sql.DataSource;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
 
 public class PostgresBaseDao {
 
-	// protected final Connection getConnection() {
-		
-    //     Connection result = null;
+  public static final String URL = "jdbc:postgresql://localhost:5432/feed_validator";
+  public static final String USERNAME = "noud";
+  public static final String PASSWORD = "test1234";
 
-	// 	try {
-	// 		InitialContext ic = new InitialContext();
-	// 		DataSource ds = (DataSource) ic.lookup("java:comp/env/jdbc/PostgresDS");
-			
-	// 		result = ds.getConnection();
-	// 	} catch(Exception e) {
-	// 		throw new RuntimeException(e);
-	// 	}
-		
-	// 	return result;
-	// }
-	
-	public static final String URL = "jdbc:postgresql://localhost:5432/feed_validator";
-    public static final String USERNAME = "noud";
-    public static final String PASSWORD = "test1234";
+  protected final Connection getConnection() {
 
-    protected Connection getConnection() throws SQLException, ClassNotFoundException {
-        Connection con = null;
-        Class.forName("org.postgresql.Driver");
-        con = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        return con;
-    } 
+    Connection result = null;
+
+    try {
+      InitialContext ic = new InitialContext();
+      DataSource ds = (DataSource) ic.lookup("java:comp/env/jdbc/PostgresDS");
+
+      result = ds.getConnection();
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+
+    return result;
+  }
 }
