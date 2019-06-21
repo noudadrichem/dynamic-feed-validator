e9093984b3356286110ca98f08f1e57404dad016
info@noudadrichem.com <info@noudadrichem.com>
2019-06-21 16:23:01 +0200

    upload feed url with error hndling

--- frontend/src/app/upload/upload.component.ts
+++ frontend/src/app/upload/upload.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from './../../environments/environment';
+const { API_URL } = environment
 
 @Component({
   selector: 'app-upload',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploadComponent implements OnInit {
 
-  constructor() { }
+  inputUrl: String = "https://bannerwise-assets.s3.eu-central-1.amazonaws.com/xml/benchy.xml"
+  isUploadSuccess: boolean = false;
+  submitted: boolean = false;
+
+  constructor(private http: HttpClient,) {}
 
   ngOnInit() {
+    console.log('moetje url')
+  }
+
+  startValidation() {
+    this.submitted = true;
+    const body = {
+      url: this.inputUrl,
+    }
+
+    this.http.post(`${API_URL}/feed/upload`, body)
+      .subscribe((data) => {
+        this.isUploadSuccess = data['success'];
+
+        setTimeout(() => {
+          this.submitted = false
+        }, 3000)
+      })
   }
 
 }
