6ef3869b512074f44804e013ed23de7d7681aa7c
info@noudadrichem.com <info@noudadrichem.com>
2019-06-26 11:05:14 +0200

    working on the socket architecture

--- src/main/java/endpoints/MessageService.java
+++ src/main/java/endpoints/MessageService.java
@@ -1,118 +1,37 @@
-// package endpoints;
+package endpoints;
 
-// import java.io.IOException;
-// import java.io.OutputStream;
-// import java.net.ServerSocket;
-// import java.net.Socket;
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObjectBuilder;
+import javax.ws.rs.*;
 
-// import javax.json.Json;
-// import javax.json.JsonArrayBuilder;
-// import javax.json.JsonObjectBuilder;
-// import javax.ws.rs.*;
-// import javax.ws.rs.core.Context;
-// import javax.ws.rs.sse.OutboundSseEvent;
-// import javax.ws.rs.sse.Sse;
-// import javax.ws.rs.sse.SseBroadcaster;
-// import javax.ws.rs.sse.SseEventSink;
+import models.Message;
+import persistence.message.PostgresMessageDao;
 
-// import models.Message;
-// import persistence.message.PostgresMessageDao;
+@Path("/message")
+public class MessageService {
+  
+  private PostgresMessageDao dao = new PostgresMessageDao();
 
-// import javax.websocket.OnClose;
-// import javax.websocket.OnMessage;
-// import javax.websocket.OnError;
-// import javax.websocket.OnOpen;
-// import javax.websocket.Session;
-// import javax.websocket.server.PathParam;
-// import javax.websocket.server.ServerEndpoint;
+  @GET
+  @Path("/all/{feedId}")
+  @Produces("application/json")
+  public String getAll(@PathParam("feedId") String feedId) throws ClassNotFoundException {
+    JsonArrayBuilder jab = Json.createArrayBuilder();
+    
+    for(Message message : dao.getAllMessagesByFeedId(feedId)) {
+      JsonObjectBuilder job = Json.createObjectBuilder();
+      job
+        .add("title", message.getTitle())
+        .add("description", message.getDescription())
+        .add("productId", message.getProductId())
+        .add("feedId", message.getfeedId())
+        .add("type", message.getType())
+        .add("hashed", message.getMessageHashCode());
 
-// import java.util.Collections;
-// import java.util.HashMap;
-// import java.util.HashSet;
-// import java.util.Set;
-// import java.util.concurrent.CopyOnWriteArraySet;
+      jab.add(job);
+    }
 
-// import org.codehaus.jettison.json.JSONException;
-// import org.codehaus.jettison.json.JSONObject;
-// import org.glassfish.jersey.server.internal.monitoring.EmptyRequestEventBuilder;
-
-// @Path("/message")
-// public class MessageService {
-
-//   private Sse sse;
-//   private EmptyRequestEventBuilder eventBuilder;
-//   private SseBroadcaster sseBroadcaster;
-
-//   @GET
-//   @Path("subscripte")
-//   @Produces("text/event-stream")
-//   public void getStockPrices(@Context SseEventSink sseEventSink /*..*/) {
-//       int lastEventId = //..;;
-//       while (running) {
-//           Stock stock = stockService.getNextTransaction(lastEventId);
-//           if (stock != null) {
-//               OutboundSseEvent sseEvent = this.eventBuilder
-//                 .name("stock")
-//                 .id(String.valueOf(lastEventId))
-//                 .mediaType(MediaType.APPLICATION_JSON_TYPE)
-//                 .data(Stock.class, stock)
-//                 .reconnectDelay(3000)
-//                 .comment("price change")
-//                 .build();
-//               sseEventSink.send(sseEvent);
-//               lastEventId++;
-//           }
-//        //..
-//       }
-//       sseEventSink.close();
-//   }
-
-//   // @Context
-//   // public void setSse(Sse sse) {
-//   //   this.sse = sse;
-//   //   this.eventBuilder = sse.newEventBuilder();
-//   //   this.sseBroadcaster = sse.newBroadcaster();
-
-//   //   // OutboundSseEvent sseEvent = this.eventBuilder
-//   //   //   .name("stock")
-//   //   //   .id(String.valueOf(lastEventId))
-//   //   //   .mediaType(MediaType.APPLICATION_JSON_TYPE)
-//   //   //   .data(Stock.class, stock)
-//   //   //   .reconnectDelay(4000)
-//   //   //   .comment("price change")
-//   //   //   .build();
-//   // }
-// }
-
-// // @ServerEndpoint("/message/{account_id}")
-// // public class MessageService {
-
-// //   private Session session;
-// //   private static Set<Message> Message = new CopyOnWriteArraySet<>();
-// //   private static HashMap<String, String> feeds = new HashMap<>();
-// //   private PostgresMessageDao dao = new PostgresMessageDao();
-
-// //   @OnOpen
-// //   public void onOpen(Session session) throws IOException {
-// //     this.session = session;
-// //     System.out.println("socket openend");
-// //   }
-
-// //   @OnMessage
-// //   public void onMessage(Session session, Message message) throws IOException {
-// //     // Handle new messages
-// //     System.out.println("socket message recieved");
-// //   }
-
-// //   @OnClose
-// //   public void onClose(Session session) throws IOException {
-// //     // WebSocket connection closes
-// //     System.out.println("socket closed");
-// //   }
-
-// //   @OnError
-// //   public void onError(Session session, Throwable throwable) {
-// //     // Do error handling here
-// //     System.out.println("socket error");
-// //   }
-// // }
+    return jab.build().toString();
+  }
+}
--- src/main/java/socket/Server.java
+++ src/main/java/socket/Server.java
@@ -1,34 +1,53 @@
 package socket;
- 
-import javax.websocket.OnClose;
-import javax.websocket.OnError;
-import javax.websocket.OnMessage;
-import javax.websocket.OnOpen;
+
+import java.io.IOException;
+
+import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
- 
+
+import models.Message;
+
 @ServerEndpoint("/socket")
 public class Server {
-     
-    @OnOpen
-    public void onOpen(){
-        System.out.println("Open Connection ...");
+
+  
+  private Session session;
+  private String sessionId; 
+
+  @OnOpen
+  public void onOpen(Session session) {
+    System.out.println("Open Connection session id=" + session.getId());
+
+    this.sessionId = session.getId();
+    this.session = session;
+  }
+
+  @OnClose
+  public void onClose() {
+    System.out.println("Close Connection ...");
+  }
+
+  @OnMessage
+  public String onMessage(String message) {
+    System.out.println("Message from the client: " + message);
+    String echoMsg = "Echo from the server : " + message;
+    return echoMsg;
+  }
+
+  @OnError
+  public void onError(Throwable e) {
+    e.printStackTrace();
+  }
+
+  public void emitMessage(Message message) throws IOException, EncodeException {
+    System.out.println("active feed Id from server=" + this.feedId);
+    if(this.feedId.equals(message.getfeedId()) && this.feedId != null) {
+      this.session.getBasicRemote().sendObject(message);
     }
-     
-    @OnClose
-    public void onClose(){
-        System.out.println("Close Connection ...");
-    }
-     
-    @OnMessage
-    public String onMessage(String message){
-        System.out.println("Message from the client: " + message);
-        String echoMsg = "Echo from the server : " + message;
-        return echoMsg;
-    }
- 
-    @OnError
-    public void onError(Throwable e){
-        e.printStackTrace();
-    }
- 
+  }
+
+  public void setFeedId(String feedId) {
+    this.feedId = feedId;
+  }
+
 }
